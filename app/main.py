83f9cc19669d98df9e7e68af4fd11844deb33541
Rafael Martinez <rafamartinez1714@gmail.com>
2025-08-24 19:42:08 -0500

    feature/error-date
    Se agrega validación para formato incorrecto de fecha.

--- app/main.py
+++ app/main.py
@@ -1,11 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Request
 from fastapi.middleware.cors import CORSMiddleware
 from app.db import models, database
+from fastapi.responses import JSONResponse
+from fastapi.exceptions import RequestValidationError
 from app.api import routes
 
 models.Base.metadata.create_all(bind=database.engine)
 
 app = FastAPI(title="CRUD Marcas")
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    errors = exc.errors()
+    for error in errors:
+        if "fecha_registro" in error["loc"]:
+            return JSONResponse(
+                status_code=422,
+                content={"detail": "La fecha de registro ingresada no es válida. Por favor, usa el formato 'YYYY-MM-DD'."},
+            )
+            
+    return JSONResponse(
+        status_code=422,
+        content={"detail": exc.errors()},
+    )
 
 app.add_middleware(
     CORSMiddleware,
